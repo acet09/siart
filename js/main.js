a8b1603d65319deeb3187aece23e65026a9fc417
Cactus <86583471+acet09@users.noreply.github.com>
2022-12-11 17:04:53 +0900

    22.12.11
    *변경점
    
    코드 마무리

--- js/main.js
+++ js/main.js
@@ -119,14 +119,14 @@ for (let i = 0; i < sidemenuBtn.length; i++) {
       items.classList.remove('active');
     });
     sidemenuBg[i].classList.add('active');
-    sidemenuBg[i].style.transform = "scale(1.0)";      
+    sidemenuBg[i].style.transform = "scale(1.0)";
     // Line
     sidemenuLine[i].classList.add('active');
     // Sub menu
     sidemenuSub.forEach((items) => {
       items.classList.remove('active');
     });
-    sidemenuSub[i].classList.add('active');  
+    sidemenuSub[i].classList.add('active');
   });
   sidemenuContents[i].addEventListener('mouseleave', (e) => {
     e.preventDefault();
@@ -135,7 +135,7 @@ for (let i = 0; i < sidemenuBtn.length; i++) {
       items.classList.remove('active');
       items.style.transform = "scale(1.035)";
     });
-    sidemenuBg[0].classList.add('active');      
+    sidemenuBg[0].classList.add('active');
     // Line
     sidemenuLine[i].classList.remove('active');
     // Sub menu
@@ -150,17 +150,17 @@ for (let i = 0; i < sidemenuBtn.length; i++) {
 const sidemenuList = document.querySelectorAll('.sidemenu__list');
 const sidemenuLink = document.querySelectorAll('.sidemenu__link');
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   for (let n = 0; n < sidemenuContents.length; n++) {
     sidemenuContents[n].style.top = ('150px');
     sidemenuContents[n].style.opacity = ('0');
     sidemenuContents[n].style.transition = ('none');
-  }    
+  }
   for (let i = 0; i < sidemenuList.length; i++) {
     sidemenuList[i].style.top = ('50px');
     sidemenuList[i].style.opacity = ('0');
     sidemenuList[i].style.transition = ('none');
-  }    
+  }
   for (let a = 0; a < sidemenuLink.length; a++) {
     sidemenuLink[a].style.top = ('150px');
     sidemenuLink[a].style.opacity = ('0');
@@ -174,22 +174,22 @@ sidemenuOpen.addEventListener('click', () => {
       sidemenuContents[n].style.top = ('0px');
       sidemenuContents[n].style.opacity = ('1');
       sidemenuContents[n].style.transition = ('all 1s');
-    },(n + 1) * 100);
-  }  
+    }, (n + 1) * 100);
+  }
   for (let i = 0; i < sidemenuList.length; i++) {
-  function sideList () {
+    function sideList() {
       sidemenuList[i].style.top = ('0px');
       sidemenuList[i].style.opacity = ('1');
       sidemenuList[i].style.transition = ('all 1s');
     }
-    setTimeout(sideList, (i + 1) * 100); 
-  }  
+    setTimeout(sideList, (i + 1) * 100);
+  }
   for (let a = 0; a < sidemenuLink.length; a++) {
     setTimeout(() => {
       sidemenuLink[a].style.top = ('0px');
       sidemenuLink[a].style.opacity = ('1');
       sidemenuLink[a].style.transition = ('all 1.3s');
-    },(a + 1) * 200);
+    }, (a + 1) * 200);
   }
 });
 
@@ -198,17 +198,17 @@ sidemenuclose.addEventListener('click', () => {
     sidemenuContents[n].style.top = ('150px');
     sidemenuContents[n].style.opacity = ('0');
     sidemenuContents[n].style.transition = ('none');
-  }    
+  }
   for (let i = 0; i < sidemenuList.length; i++) {
     sidemenuList[i].style.top = ('50px');
     sidemenuList[i].style.opacity = ('0');
     sidemenuList[i].style.transition = ('none');
-  }    
+  }
   for (let a = 0; a < sidemenuLink.length; a++) {
     sidemenuLink[a].style.top = ('150px');
     sidemenuLink[a].style.opacity = ('0');
     sidemenuLink[a].style.transition = ('none');
-  }    
+  }
 });
 
 
@@ -250,7 +250,7 @@ var swiperBullet = new Swiper(".mySwiper-story", {
   thumbs: {
     swiper: thumbs,
   },
-});    
+});
 
 
 // *** SI COLLECTION ***
@@ -265,7 +265,7 @@ var swiper__new = new Swiper(".mySwiper-new", {
     nextEl: ".new-next",
     prevEl: ".new-prev",
   },
-  observer: true, 
+  observer: true,
   observeParents: true,
 });
 
@@ -280,7 +280,7 @@ var swiper__best = new Swiper(".mySwiper-best", {
     nextEl: ".best-next",
     prevEl: ".best-prev",
   },
-  observer: true, 
+  observer: true,
   observeParents: true,
 });
 
@@ -288,7 +288,7 @@ var swiper__best = new Swiper(".mySwiper-best", {
 const tab__btn = document.querySelectorAll('.tab__text>a');
 const tab__slide = document.querySelectorAll('.tab__swiper');
 
-for(let i = 0; i < tab__btn.length; i++) {
+for (let i = 0; i < tab__btn.length; i++) {
   tab__btn[i].addEventListener('click', (e) => {
     e.preventDefault();
     tab__btn.forEach((item) => {
@@ -314,7 +314,7 @@ var swiperBullet = new Swiper(".mySwiper-news", {
       return '<div class="' + className + '"><strong> EVENT </strong><br><span>' + (news__bullet[index]) + '</span></div>';
     }
   },
-});    
+});
 
 
 //  *** footer family menu ***
@@ -324,7 +324,7 @@ const family__menu = document.querySelector(".footer__listbox");
 
 family__open.addEventListener('click', (e) => {
   e.preventDefault();
-  family__menu.style.height = ('140px');  
+  family__menu.style.height = ('140px');
 });
 
 family__close.addEventListener('click', (e) => {
@@ -338,7 +338,7 @@ const topbtn = document.getElementById('topbtn');
 const topbtn__text = document.querySelector('.top--move');
 topbtn__text.onclick = (e) => {
   e.preventDefault();
-  window.scrollTo({ top: 0, behavior: "smooth" });  
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 const topbtn__open = document.querySelector('.side--open');
@@ -362,8 +362,7 @@ const banner__height = banner.getBoundingClientRect().height;
 const footer = document.getElementById('footer');
 const footer__height = footer.getBoundingClientRect().height;
 const body__height = document.body.scrollHeight;
-const topbtn__sub = (banner__height + 180 + footer__height);
-
+const topbtn__sub = (banner__height + footer__height + 270);
 document.addEventListener('scroll', () => {
   if (window.scrollY > '0') {
     topbtn__text.classList.add('hide');
@@ -371,26 +370,41 @@ document.addEventListener('scroll', () => {
     topbtn__text.classList.remove('hide');
   }
 
-  if (window.scrollY >= body__height - topbtn__sub) {
+  if (window.scrollY > body__height - topbtn__sub) {
     topbtn.classList.add('active');
-  } else {  
+  } else {
     topbtn.classList.remove('active');
   }
 });
 
-// browser refresh
-window.onload = function() {
-  body.classList.remove('scroll-down');
-  setTimeout (function(){scrollTo(0,0);}, 100);
+
+// *** Intersection Observer *** 
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: .5,
 }
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, options);
+const titleList = document.querySelectorAll('.observer');
+titleList.forEach(el => observer.observe(el));
 
 
-// 구현해야 할것
-// 1. 각 영역에 진입시 숨겨진 메뉴 활성화
-// 각 영역의 top좌표를 getboundingclientrect로 출력 받는
-// 윈도우 top 좌표와 해당위치의 좌표가 일치하면 애니메이션 발생
-
-
+// *** browser refresh ***
+window.onload = function () {
+  body.classList.remove('scroll-down');
+  setTimeout(function () { 
+    titleList.forEach((items) => {
+      items.classList.remove('active');
+    });
+    scrollTo(0, 0); 
+  }, 100);
+}
 
 
 
